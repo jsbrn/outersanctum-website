126e00bfbf4b2cceea050b2b46baeab41df05491
Jeremy <23155614+jsbrn@users.noreply.github.com>
2026-03-21 14:08:05 -0400

    Cleanup

--- assets/email-protection.js
+++ assets/email-protection.js
@@ -1,51 +1,45 @@
-'use strict';
+"use strict";
 
-document.addEventListener('DOMContentLoaded', function ()
-{
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("no-js").remove();
 
-    document.getElementById("no-js").remove()
-    
-	const listener = new Listener();
-	listener.decode = function ()
-	{
-
-		const button = document.getElementById('email-address');
+    const listener = new Listener();
+    listener.decode = function () {
+        const button = document.getElementById("email-address");
         const obfuscated = "apogee . sheep ! com";
-        const revealed = obfuscated.replace("apogee", "contact").replace(".", "@").replaceAll(" ", "").replace("sheep", "outersanctum").replace("!", ".").replace("com", "ca");
-		button.setAttribute("href", `mailto:${revealed}`);
-        button.innerText = revealed
-	}
+        const revealed = obfuscated
+            .replace("apogee", "contact")
+            .replace(".", "@")
+            .replaceAll(" ", "")
+            .replace("sheep", "outersanctum")
+            .replace("!", ".")
+            .replace("com", "ca");
+        button.setAttribute("href", `mailto:${revealed}`);
+        button.innerText = revealed;
+    };
 
-	listener.on();
+    listener.on();
 });
 
-
-// Listener
-
-function Listener ()
-{
-}
+function Listener() {}
 
 Listener.prototype.decode = null;
 
-Listener.prototype.on = function ()
-{
-	this.listener = this.__onInteraction.bind(this);
+Listener.prototype.on = function () {
+    this.listener = this.__onInteraction.bind(this);
 
-	document.addEventListener('mouseenter', this.listener, true);
-	document.addEventListener('focus', this.listener, true);
-}
+    document.addEventListener("mouseenter", this.listener, true);
+    document.addEventListener("focus", this.listener, true);
+};
 
-Listener.prototype.off = function ()
-{
-	document.removeEventListener('mouseenter', this.listener, true);
-	document.removeEventListener('focus', this.listener, true);
+Listener.prototype.off = function () {
+    document.removeEventListener("mouseenter", this.listener, true);
+    document.removeEventListener("focus", this.listener, true);
 
-	delete this.listener;
-}
+    delete this.listener;
+};
 
-Listener.prototype.__onInteraction = function ()
-{
-	this.off();
-	this.decode();
-}
+Listener.prototype.__onInteraction = function () {
+    this.off();
+    this.decode();
+};
